ebf434a8309a9c7c9bfc320bfe5161e721604ab4
Subhrojit Mondal <subhromond@gmail.com>
2021-04-30 22:59:11 +0530

    Browser

--- 06_Browser_Todo/activity/script.js
+++ 06_Browser_Todo/activity/script.js
@@ -1,21 +1,22 @@
 let colorBtn = document.querySelectorAll(".filter_color");
 let mainContainer = document.querySelector(".main-container");
 let body = document.body;
-let plusBtn = document.querySelector(".fa-plus")
-for (let i = 0; i < colorBtn.length; i++) {
-    colorBtn[i].addEventListener("click", function (e) {
-        let color = colorBtn[i].classList[1];
-        mainContainer.style.backgroundColor = color;
+let plusBtn = document.querySelector(".fa-plus");
+let delBtn = document.querySelector(".fa-times");
+let deleteState = false;
 
-    })
-}
+
 
 plusBtn.addEventListener("click", createModal);
+delBtn.addEventListener("click", setDeleteState);
+
 
 function createModal() {
-    let modal_container = document.createElement("div");
-    modal_container.setAttribute("class", "modal_container");
-    modal_container.innerHTML = `<div class="input_container">
+    let modal_container = document.querySelector(".modal_container");
+    if (modal_container == null) {
+        modal_container = document.createElement("div");
+        modal_container.setAttribute("class", "modal_container");
+        modal_container.innerHTML = `<div class="input_container">
     <textarea class="modal_input" placeholder="Enter Your Text Here"></textarea>
 </div>
 <div class="modal_filter_container">
@@ -25,45 +26,82 @@ function createModal() {
     <div class="filter black"></div>
 
 </div>`
-    body.appendChild(modal_container);
-    handleModal(modal_container);
+        body.appendChild(modal_container);
+        handleModal(modal_container);
+    }
+    let textarea=document.querySelector(".modal_input");
+    textarea.value="";
 
 }
 
 function handleModal(modal_container) {
-    let cColor="black";
+    let cColor = "black";
     let borderSelect = document.querySelectorAll(".modal_filter_container .filter");
     borderSelect[3].classList.add("border");
     for (let i = 0; i < borderSelect.length; i++) {
         borderSelect[i].addEventListener("click", function (e) {
-            borderSelect.forEach((filter)=>{
+            borderSelect.forEach((filter) => {
                 filter.classList.remove("border");
 
             })
             borderSelect[i].classList.add("border");
-            cColor=borderSelect[i].classList[1];
+            cColor = borderSelect[i].classList[1];
         })
     }
-    let textArea=document.querySelector(".modal_input");
-    textArea.addEventListener("keypress",function(e){
-        if(e.key=="Enter" && textArea.value!=""){
+    let textArea = document.querySelector(".modal_input");
+    textArea.addEventListener("keypress", function (e) {
+        if (e.key == "Enter" && textArea.value != "") {
             modal_container.remove();
-            createTask(cColor,textArea.value)
+            createTask(cColor, textArea.value)
         }
     })
 
 }
-function createTask(cColor,task){
-    let taskConatiner=document.createElement("div")
-    taskConatiner.setAttribute("class","task_container");
-    taskConatiner.innerHTML=`<div class="task_filter ${cColor}"></div>
-    <div class="task_desc_container">
-        <h3 class="uid">#example</h3>
+function createTask(cColor, task) {
+    let taskContainer = document.createElement("div")
+    var uid = new ShortUniqueId();
+    taskContainer.setAttribute("class", "task_container");
+    taskContainer.innerHTML = `<div class="task_filter ${cColor}"></div>
+    <div class="task_desc_container" contenteditable="true">
+        <h3 class="uid">#${uid()}</h3>
         <div class="task_desc">${task}</div>
     </div>
 </div>`;
-mainContainer.appendChild(taskConatiner);
+    mainContainer.appendChild(taskContainer);
+    let taskFilter = taskContainer.querySelector(".task_filter");
+    taskFilter.addEventListener("click", changeColor);
+    taskContainer.addEventListener("click", deleteTask)
 
+}
+
+function changeColor(e) {
+    let taskFilter = e.currentTarget;
+    let color = ["pink", "blue", "green", "black"];
+    let cColor = taskFilter.classList[1];
+    let idx = color.indexOf(cColor);
+    let newColorIdx = (idx + 1) % 4;
+    taskFilter.classList.remove(cColor);
+    taskFilter.classList.add(color[newColorIdx]);
+
+}
+
+function setDeleteState(e) {
+    let crossBtn = e.currentTarget;
+    let parent = crossBtn.parentNode;
+    if (deleteState == false) {
+        parent.classList.add("active")
+    } else {
+        parent.classList.remove("active");
+    }
+    deleteState = !deleteState;
+
+}
+
+function deleteTask(e) {
+    let taskContainer = e.currentTarget;
+    if (deleteState) {
+        taskContainer.remove();
+    }
 
 }
 
--- 06_Browser_Todo/raw/todo/script.js
+++ 06_Browser_Todo/raw/todo/script.js
@@ -1,6 +1,24 @@
 let input=document.querySelector(".input_box");
 let ul=document.querySelector(".task-list");
+let arr=[];
 
+if(localStorage.getItem("allTask")){
+    let stringArr=localStorage.getItem("allTask");
+
+    let arr=JSON.parse(stringArr);
+    for(let i=0;i<arr.length;i++){
+        let li=document.createElement("li");
+        li.innerText=arr[i];
+        li.addEventListener("dblclick",function(e){
+            li.remove();
+            let idx=arr.indexOf(li.innerText)
+            localStorage.removeItem(i);
+        });
+        //sets any attribute to the element
+        li.setAttribute("class","task");
+        ul.appendChild(li);
+    }
+}
 input.addEventListener("keypress",function(e){
     //e-->event obeject containing propertirs of the respective evenet
     //console.log("pressed some key");
@@ -13,6 +31,9 @@ input.addEventListener("keypress",function(e){
         //creates any html element
         let li=document.createElement("li");
         li.innerText=task;
+        arr.push(task);
+        let stringArr=JSON.stringify(arr);
+        localStorage.setItem("allTask",stringArr);
         //adds eventlistner to li node only when created
         li.addEventListener("dblclick",function(e){
             li.remove();
